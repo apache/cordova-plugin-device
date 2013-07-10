50d4d062683702a0bc87fd7b6f6736895f69fa4b
Andrew Grieve <agrieve@chromium.org>
2013-07-10 09:31:32 -0400

    [CB-4038] - Remove o.a.c.api namespace

--- src/android/Device.java
+++ src/android/Device.java
@@ -21,10 +21,10 @@ package org.apache.cordova.core;
 import java.util.TimeZone;
 
 import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.api.CallbackContext;
-import org.apache.cordova.api.CordovaPlugin;
-import org.apache.cordova.api.LOG;
-import org.apache.cordova.api.CordovaInterface;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.LOG;
+import org.apache.cordova.CordovaInterface;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
