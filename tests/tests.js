f94f1d4e86d546f81c95ac478123e15c140f46c9
Edna Morales <ednamorales@ednas-mbp-2.raleigh.ibm.com>
2014-07-24 15:09:29 -0400

    added status box and documentation to manual tests

--- tests/tests.js
+++ tests/tests.js
@@ -66,7 +66,29 @@ exports.defineAutoTests = function() {
 };
 
 exports.defineManualTests = function(contentEl, createActionButton) {
-  createActionButton('Dump device', function() {
-    console.log(JSON.stringify(window.device, null, '\t'));
-  });
+  var logMessage = function (message, color) {
+        var log = document.getElementById('info');
+        var logLine = document.createElement('div');
+        if (color) {
+            logLine.style.color = color;
+        }
+        logLine.innerHTML = message;
+        log.appendChild(logLine);
+    }
+
+    var clearLog = function () {
+        var log = document.getElementById('info');
+        log.innerHTML = '';
+    }
+
+    var device_tests = '<h3>Press Dump Device button to get device information</h3>' +
+        '<div id="dump_device"></div>' +
+        'Expected result: Status box will get updated with device info. (i.e. platform, version, uuid, model, etc)';
+
+    contentEl.innerHTML = '<div id="info"></div>' + device_tests;
+
+    createActionButton('Dump device', function() {
+      clearLog();
+      logMessage(JSON.stringify(window.device, null, '\t'));
+    }, "dump_device");
 };
