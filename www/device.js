c51f9e3daa3c8dbfede81bba95a1fc8f31038783
Raphael von der Grün <raphinesse@gmail.com>
2019-10-13 22:03:30 +0200

    Remove obsolete dependency on cordova/utils

--- www/device.js
+++ www/device.js
@@ -21,7 +21,6 @@
 
 var argscheck = require('cordova/argscheck');
 var channel = require('cordova/channel');
-var utils = require('cordova/utils');
 var exec = require('cordova/exec');
 var cordova = require('cordova');
 
