1c8e8155931124a40a1c8d8c3acb189a36898754
Mark Veenstra <mark@mobilea.nl>
2020-01-02 10:35:06 +0100

    #113 (browser) - Properly detect new Edge Chromium version as Edge instead of Chrome

--- src/browser/DeviceProxy.js
+++ src/browser/DeviceProxy.js
@@ -37,8 +37,16 @@ function getBrowserInfo (getModel) {
     var returnVal = '';
     var offset;
 
-    if ((offset = userAgent.indexOf('Edge')) !== -1) {
-        returnVal = (getModel) ? 'Edge' : userAgent.substring(offset + 5);
+    if ((offset = userAgent.indexOf('Edg')) !== -1) {
+        if (getModel) {
+            returnVal = 'Edge';
+        } else {
+            if ((offset = userAgent.indexOf('Edge')) !== -1) {
+                returnVal = userAgent.substring(offset + 5);
+            } else {
+                returnVal = userAgent.substring(offset + 4);
+            }
+        }
     } else if ((offset = userAgent.indexOf('Chrome')) !== -1) {
         returnVal = (getModel) ? 'Chrome' : userAgent.substring(offset + 7);
     } else if ((offset = userAgent.indexOf('Safari')) !== -1) {
